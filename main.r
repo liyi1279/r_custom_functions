3d543ded1d12e221343a8ce6464d37410e42f003
liyi <liyi1279@hotmail.com>
2019-12-05 13:17:22 +0900

    add CheckPackages

---
+++ main.r
@@ -0,0 +1,16 @@
+## copy from website
+## usage example
+#packages <- c('ggplot2', 'afex')
+#CheckPackages(packages)
+CheckPackages <- function(pkg){
+  # check if biocManager install
+  pkg.manager <- ('BiocManager' %in% installed.packages()[,'Package'])
+  if(!pkg.manager) install.packages('BioManager')
+    
+  # check if input package installed
+  new.pkg <- pkg[!(pkg %in% installed.packages()[, 'Package'])]
+  if(length(new.pkg)) BiocManager::install('afex')
+  
+  # load all packages
+  sapply(pkg, require, character.only=TRUE)
+}
